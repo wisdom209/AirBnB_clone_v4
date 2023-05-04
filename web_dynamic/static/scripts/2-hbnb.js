e83f1ce4317cc9508b251915352e92d966f15c89
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-05-04 15:41:11 +0100

    Feat: add api status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,68 @@
+/* Jquery handling amenities checkbox */
+let checked_amenity_ids = [];
+let checked_amenity_names = []
+
+$(document).ready(function () {
+	/* ************************* */
+	/* handle amenity check box */
+	const inputs = $('input[type="checkbox"]')
+
+	inputs.each(function (index, input) {
+		$(this).on('change', (e) => {
+			const amenity_id = e.target.dataset.id;
+			const amenity_name = e.target.dataset.name;
+
+			/* add amenities to list */
+			if (e.target.value === 'on') {
+				if (!(checked_amenity_ids.includes(amenity_id))) {
+					checked_amenity_ids.push(amenity_id)
+					checked_amenity_names.push(amenity_name)
+				}
+			}
+			if (e.target.value === 'off') {
+				if (checked_amenity_ids.includes(amenity_id)) {
+					checked_amenity_ids = checked_amenity_ids.filter(value => value !== amenity_id)
+					checked_amenity_names = checked_amenity_names.filter(value => value !== amenity_name);
+				}
+			}
+
+			/* toggle checkbox values */
+			if (e.target.value === 'on') {
+				e.target.value = 'off'
+			}
+			else {
+				e.target.value = 'on'
+			}
+
+			/* update h4 tag */
+			$('.amenities h4').html('&nbsp;');
+			let amenityText = checked_amenity_names.join(', ');
+			if (checked_amenity_names.length === 0) {
+				$('.amenities h4').html('&nbsp;');
+			}
+			else if (amenityText.length > 17) {
+				amenityText = amenityText.slice(0, 17) + '...';
+				$('.amenities h4').text(amenityText);
+			}
+			else {
+				$('.amenities h4').text(amenityText);
+			}
+		})
+	});
+
+	/* *************************************** */
+	/* handle http return status */
+	const statusElem = $('div#api_status')
+	let apiStatus = ""
+	$.get('http://0.0.0.0:5001/api/v1/status', (res) => {
+		console.log(res);
+		if (res.status === "OK") {
+			statusElem.addClass('available');
+			statusElem.removeAttr('id')
+		}
+		else {
+			statusElem.attr('id', 'api_status');
+			statusElem.removeClass('available');
+		}
+	})
+})
